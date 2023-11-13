f5d561be9c1428f4c734bad38a6fd7cda6796a60
Fernando Campo <campofernando594@gmail.com>
2023-11-13 13:08:52 -0300

    Add ML models

--- data-pre-processing/GetSensorDataService.py
+++ data-pre-processing/GetSensorDataService.py
@@ -1,14 +1,12 @@
 import requests
 import pandas as pd
 import datetime as dt
+import json
 
 class GetSensorDataService:
-    def __init__(self, host, port, endpoint) -> None:
-        self.__endpoint = 'http://' + host + ':' + str(port) + endpoint
+    def __init__(self, host, port) -> None:
+        self.__host = 'http://' + host + ':' + str(port)
 
-    def get_json_data_from_sensor_id(self, id):
-        return requests.get(self.__endpoint + str(id))
-    
     def get_data_from_file(self, filename, sensor_name):
         df = pd.read_csv(filename)
         date_time_col = ['Year','Month','Day','Hour','Minute','Second']
@@ -21,3 +19,97 @@ class GetSensorDataService:
         path = "data/input/"
         df.to_csv(path + sensor_name + 'web_dataframe.csv') 
         return df
+    
+    def get_samples_by_sensor(self, sensor_id):
+        """
+        Returns all data points in a sensor
+        Parameters
+        ----------
+        sensor_id : String
+            The id of the sensor on Renovar API.
+
+        Returns
+        -------
+        response_dataframe : Pandas Dataframe
+            A pandas dataframe with Datetime indexes containing all the data points of the sensor.
+            
+            index='date'- Datetime indexes
+            columns='measuring'
+        """
+
+        ENDPOINT = "/sample/sensor/all/"
+        REQUEST = self.__host + ENDPOINT
+        response_json = requests.get(REQUEST + str(sensor_id))
+        response_dict = json.loads(response_json.content)
+        response_dataframe = pd.DataFrame.from_dict(response_dict)
+        return self.__prepare_date_time_dataframe__(response_dataframe[["date","measuring"]])
+
+    def get_samples_by_sensor_in_range(self, sensor_id, start_date, end_date):
+        """
+        Returns all data points in a sensor within a date range
+        Parameters
+        ----------
+        sensor_id : String
+            The id of the sensor on Renovar API.
+        start_date: String
+            The start date with the format YYYY-MM-DD
+        end_date: String
+            The end date with the format YYYY-MM-DD
+
+        Returns
+        -------
+        response_dataframe : Pandas Dataframe
+            A pandas dataframe with Datetime indexes containing all the data points of the sensor.
+            
+            index='date'- Datetime indexes
+            columns='measuring'
+        """
+
+        ENDPOINT = "/sample/sensor/range/?sensorID=" + str(sensor_id) + "&startDate=" + start_date + "&endDate=" + end_date
+        REQUEST = self.__host + ENDPOINT 
+        response_json = requests.get(REQUEST)
+        response_dict = json.loads(response_json.content)
+        content = response_dict['content']
+        measuring_list = [item['measuring'] for item in content]
+        date_list = [item['date'] for item in content]
+        measuring_dataframe = pd.DataFrame({'measuring': measuring_list, 'date': date_list})
+        return self.__prepare_date_time_dataframe__(measuring_dataframe)
+    
+    def __prepare_date_time_dataframe__(self, dataframe):
+        """
+        Receives a dataframe with 'date' and 'measuring' columns
+        Returns dataframe with DateTime indexes and resampled to a period of 15 mins
+        """
+        measuring_dataframe = dataframe
+        measuring_dataframe['date'] = (pd.to_datetime(measuring_dataframe['date'], infer_datetime_format=True))
+
+        # Resample data with 15 mins period and create sensor dataframe
+        measuring_dataframe = measuring_dataframe.sort_values(by='date', ascending=True).reset_index().drop(columns='index')
+        measuring_dataframe.index = measuring_dataframe['date']
+        measuring_dataframe = measuring_dataframe.drop(columns=['date'])
+        return measuring_dataframe 
+
+    def get_last_sample_of_sensor(self, sensor_id):
+        """
+        Returns the last sample of a sensor
+        Parameters
+        ----------
+        sensor_id : String
+            The id of the sensor on Renovar API.
+
+        Returns
+        -------
+        date : String
+            The datetime of the sample
+            
+        measuring: Double
+            The value of the sample
+        """
+
+        ENDPOINT = "/sample/sensor/last/" + str(sensor_id)
+        REQUEST = self.__host + ENDPOINT 
+        response_json = requests.get(REQUEST)
+        response_dict = json.loads(response_json.content)
+        measuring = response_dict['measuring']
+        date = response_dict['date']
+        return date, measuring
